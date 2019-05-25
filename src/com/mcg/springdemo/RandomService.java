42d5baa4d7f0c25a895fac3f8e9ce3430c447f73
Muhammetcan Gul <muhammetcangl@gmail.com>
2019-05-25 14:20:50 +0300

    Adding Qualifier annotatiton

---
+++ src/com/mcg/springdemo/RandomService.java
@@ -0,0 +1,12 @@
+package com.mcg.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomService implements FortuneService {
+
+    @Override
+    public String getFortune() {
+        return null;
+    }
+}
