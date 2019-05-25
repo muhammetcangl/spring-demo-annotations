7a1c6827256312366e81bca902de1b011c41f169
Muhammetcan Gul <muhammetcangl@gmail.com>
2019-05-25 16:15:42 +0300

    Adding properties file and injecting values.

--- src/com/mcg/springdemo/SwimCoach.java
+++ src/com/mcg/springdemo/SwimCoach.java
@@ -1,9 +1,17 @@
 package com.mcg.springdemo;
 
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SwimCoach implements Coach {
 
     private FortuneService fortuneService;
+
+    @Value("${foo.email}")
+    private String email;
+
+    @Value("${foo.team}")
+    private String team;
 
     public SwimCoach(FortuneService theFortuneService){
         fortuneService = theFortuneService;
@@ -17,5 +25,13 @@ public class SwimCoach implements Coach {
     @Override
     public String getDailyFortune() {
         return fortuneService.getFortune();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTeam() {
+        return team;
     }
 }
