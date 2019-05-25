b8241206bf1e9e1107124bc16fc8ddb5c6e32d36
Muhammetcan Gul <muhammetcangl@gmail.com>
2019-05-25 15:44:29 +0300

    Adding @Configuration and @ComponentScan annotations.

---
+++ src/com/mcg/springdemo/JavaConfigDemoApp.java
@@ -0,0 +1,26 @@
+package com.mcg.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class JavaConfigDemoApp {
+
+    public static void main(String[] args) {
+
+        //read spring config java class
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(SportConfig.class);
+
+        //get the bean from spring container
+        Coach theCoach = context.getBean("tennisCoach", Coach.class);
+
+        //call a method
+        System.out.println(theCoach.getDailyWorkout());
+
+        //call method to get daily fortune
+        System.out.println(theCoach.getDailyFortune());
+
+        //close the context
+        context.close();
+    }
+}
---
+++ src/com/mcg/springdemo/SportConfig.java
@@ -0,0 +1,9 @@
+package com.mcg.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.mcg.springdemo")
+public class SportConfig {
+}
